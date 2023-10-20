946352fcda0875ba86746a839eb427ba07bc92ab
+dennisngangairungu <+>
2023-10-20 13:15:46 +0300

    refined the display of movies and its details

--- src/script.js
+++ src/script.js
@@ -56,9 +56,13 @@ function displayCards(filmsArr){
     <div class="card" style="width: 18rem;">
     <img src="${card.poster}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">${card.title}</h5>
+      <p class="card-text">${card.description}</p>
+      <p class="card-text">Run Time : ${card.runtime}</p>
+      <p class="card-text">Show Time : ${card.showtime}</p>
+      <p class="card-text">Capacity : ${card.capacity}</p>
+      <p class="card-text">Tickets Sold : ${card.tickets_sold}</p>
+      <button class="buy-ticket">Buy Ticket</button>
     </div>
   </div>
     `
