32486f3226725b6dfd76ef29adb312f870a16167
+dennisngangairungu <+>
2023-10-20 12:01:00 +0300

    bootstrap is working

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+//initialize an array globaly and copy the array in db to a new array using spread operator
+let filmsArr=[]
 // to make the web not interactive  an event listner is added "Domcontentloaded"
 document.addEventListener('DOMContentLoaded',()=>{
     getElements()
@@ -5,8 +7,42 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 // this is a get request made to the db
 function getElements(){
-    fetch('http://localhost:3001/films')
+    fetch('http://localhost:3000/films')
         .then(res => res.json())
-        .then(data => console.log(data))
-
+        .then(data => {
+            filmsArr=[...data]
+            //console.log(filmsArr)
+            displayDropDown(filmsArr)
+        
+        
+        })
+        //console.log(filmsArr)
+        
 }
+
+function displayDropDown(filmsArr){
+    console.log(filmsArr)
+    const dropDown=document.getElementById('drop-down-movies')
+    dropDown.innerHTML+=`
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Available Movies
+  </button> <br> 
+  <ul class="dropdown-menu dropdown-menu-dark">
+    <li id="li" class="dropdown-item active" >Top Play</li>
+    <li class="dropdown-item" >Another action</li>
+    
+  </ul>
+</div>  <br>
+    `
+const btn=document.querySelector('#li')
+btn.addEventListener('click', ()=> {
+  filmsArr.forEach( title => {
+    dropDown.innerHTML+=`
+      
+          <li class="movie-name">${title.title}</li> <br> <br>
+    `
+  })
+})
+  
+}   
